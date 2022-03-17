bb5d3a6179baf20accc7c7c6976759cc8af0489f
hyun <coderhyun476@gmail.com>
2022-03-17 19:32:03 +0900

    init example

---
+++ example/App.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
